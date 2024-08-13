a3cce1173fe1b9a2206876257a7ee2714cc9f2f0
Dennis Huber <dennis.huber@hs-heilbronn.de>
2024-08-13 10:36:37 +0200

    [ADD] Method to reduce sample size per algorithm family (compression method)

--- feature_extraction.py
+++ feature_extraction.py
@@ -41,6 +41,37 @@ def downsample(group, interval):
     })
 
 
+def reduce_sample_size(df_samples: pd.DataFrame, num_samples_to_keep: int):
+    """Reduces the number of samples in a DataFrame for each algorithm family, based on sequential series IDs.
+
+    This function creates a sequential series ID within each algorithm family (identified by `algo_family_id`)
+    that increments when the `series_id` changes. It then filters the DataFrame to keep only the specified
+    number of samples per algorithm family.
+
+    :param df_samples: The DataFrame containing the samples. Must include 'algo_family_id' and 'series_id' columns.
+    :param num_samples_to_keep: The number of samples to keep per algorithm family.
+
+    :returns: A DataFrame containing only the reduced number of samples for each algorithm family.
+
+    :raises ValueError: If any algorithm family does not have enough samples to meet the `num_samples_to_keep` requirement.
+    """
+    # Creating incrementing `series_id` (counter) per `algo_family_id`
+    df_samples['series_id_change'] = df_samples.groupby('algo_family_id')['series_id'].transform(
+        lambda x: x != x.shift())  # Identify change in `algo_family`
+
+    # Create the new column with sequential integers per `algo_family_id` that increment only when `series_id` changes
+    df_samples['sequential_series_id'] = df_samples.groupby('algo_family_id')['series_id_change'].cumsum()
+    df_samples.drop(columns='series_id_change', inplace=True)
+
+    # Only keep the desired amount of samples
+    if (df_samples.groupby('algo_family_id')['sequential_series_id'].max() < num_samples_to_keep).any():
+        # Does not contain enough samples for all `algo_family_id`
+        raise ValueError(
+            fr'The data does not contain enough samples for all algorithm families to reduce the sample size to {num_samples_to_keep} per family. Choose a smaller number of samples to keep.')
+    else:
+        return df_samples.loc[df_samples.sequential_series_id <= num_samples_to_keep]
+
+
 def load_and_preprocess_data(tsad_results_path, time_series_metadata_path):
     """Load and preprocess the data from specified file paths.
 
@@ -81,30 +112,33 @@ def load_and_preprocess_data(tsad_results_path, time_series_metadata_path):
     df_feature_extraction = df_feature_extraction.explode(['test_data', 'time_step'])
     df_feature_extraction[['test_data', 'time_step']] = df_feature_extraction[['test_data', 'time_step']].apply(
         pd.to_numeric, errors='coerce')
+    df_feature_extraction['series_id'] = (df_feature_extraction['time_step'] == 0).cumsum()
 
-    return df_feature_extraction
+    return df_feature_extraction.reset_index(drop=True)
 
 
-def main(tsad_results_path, time_series_metadata_path, downsampling_interval, output_path):
+def main(tsad_results_path, time_series_metadata_path, downsampling_interval, reduced_sample_size, output_path):
     """Main function to load data, downsample, and extract features.
 
     :param tsad_results_path: Path to the TSAD evaluation results CSV file.
     :param time_series_metadata_path: Path to the time series metadata CSV file.
     :param downsampling_interval: Interval for downsampling the time series.
+    :param reduced_sample_size: Number of samples to keep for each algorithm family.
     :param output_path: Path to save the extracted features CSV file.
 
     :returns: None. The extracted features are saved to the specified output path.
     """
     df_feature_extraction = load_and_preprocess_data(tsad_results_path, time_series_metadata_path)
 
-    df_feature_extraction['series_id'] = (df_feature_extraction['time_step'] == 0).cumsum()
-
     tqdm.pandas(desc="Downsampling time series data")
-    df_downsampled = df_feature_extraction.groupby(['series_id']).progress_apply(downsample,
-                                                                                 interval=downsampling_interval).reset_index(
-        drop=True)
 
-    extracted_features = extract_features(df_downsampled, column_id='algo_family_id', column_sort='time_step')
+    if downsampling_interval != 0:
+        df_feature_extraction = df_feature_extraction.groupby(['series_id']).progress_apply(downsample,interval=downsampling_interval).reset_index(drop=True)
+
+    if reduced_sample_size != 0:
+        df_feature_extraction = reduce_sample_size(df_feature_extraction, reduced_sample_size)
+
+    extracted_features = extract_features(df_feature_extraction, column_id='algo_family_id', column_sort='time_step')
     extracted_features.to_csv(output_path)
 
 
@@ -114,11 +148,13 @@ if __name__ == "__main__":
                         help="Path to the TSAD evaluation results CSV file.")
     parser.add_argument("--time-series-metadata", type=str, default='datasets/GutenTAG/datasets.csv',
                         help="Path to the time series metadata CSV file.")
-    parser.add_argument("--downsampling-interval", type=int, default=5,
+    parser.add_argument("--downsampling-interval", type=int, default=0,
                         help="Interval for downsampling the time series.")
+    parser.add_argument("--reduced-sample-size", type=int, default=0,
+                        help="Number of samples to keep for each algorithm family.")
     parser.add_argument("--output-path", type=str, default='datasets/features.csv',
                         help="Path to save the extracted features CSV file.")
 
     args = parser.parse_args()
 
-    main(args.tsad_results, args.time_series_metadata, args.downsampling_interval, args.output_path)
+    main(args.tsad_results, args.time_series_metadata, args.downsampling_interval, args.reduced_sample_size, args.output_path)
